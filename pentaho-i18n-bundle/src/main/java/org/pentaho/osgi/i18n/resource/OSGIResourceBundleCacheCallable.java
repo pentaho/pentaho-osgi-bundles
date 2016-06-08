7a813acaee493ca80d6bbc2e0cc5b06ad0b50322
candentira <viktoryia_burchyk@epam.com>
2016-06-08 12:48:51 +0300

    [BACKLOG-8306] - make properties files be merged according to Locale and Priority

--- pentaho-i18n-bundle/src/main/java/org/pentaho/osgi/i18n/resource/OSGIResourceBundleCacheCallable.java
+++ pentaho-i18n-bundle/src/main/java/org/pentaho/osgi/i18n/resource/OSGIResourceBundleCacheCallable.java
@@ -22,11 +22,19 @@
 
 package org.pentaho.osgi.i18n.resource;
 
+import org.pentaho.osgi.i18n.settings.OSGIResourceNameComparator;
 import org.pentaho.osgi.i18n.settings.OSGIResourceNamingConvention;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.concurrent.Callable;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 
 /**
@@ -41,36 +49,74 @@ public class OSGIResourceBundleCacheCallable implements Callable<Map<String, OSG
 
   @Override public Map<String, OSGIResourceBundle> call() throws Exception {
     Map<String, OSGIResourceBundleFactory> factoryMap =
-      new HashMap<String, OSGIResourceBundleFactory>();
+      new TreeMap<>( new OSGIResourceNameComparator() );
     // Select only bundles with highest priority
     for ( Map<String, OSGIResourceBundleFactory> bundleMap : configMap.values() ) {
       for ( Map.Entry<String, OSGIResourceBundleFactory> entry : bundleMap.entrySet() ) {
+        OSGIResourceBundleFactory pathToFactoryMap = entry.getValue();
         String key = entry.getValue().getPropertyFilePath();
-        OSGIResourceBundleFactory pathToFactoryMap = factoryMap.get( key );
-        if ( pathToFactoryMap == null || pathToFactoryMap.getPriority() < entry.getValue().getPriority() ) {
-          pathToFactoryMap = entry.getValue();
-        }
         factoryMap.put( key, pathToFactoryMap );
       }
     }
 
     // Create bundles from factories
     Map<String, OSGIResourceBundle> result = new HashMap<String, OSGIResourceBundle>();
-    for ( Map.Entry<String, OSGIResourceBundleFactory> factoryMapEntry : factoryMap.entrySet() ) {
-      OSGIResourceBundle resultKeyBundles;
-      OSGIResourceBundleFactory nameToFactoryEntry = factoryMapEntry.getValue();
+    Set<String> keys = factoryMap.keySet();
+    SortedMap<String, OSGIResourceBundleFactory> factoryMapCopy =
+      new TreeMap<>( new OSGIResourceNameComparator() );
+    factoryMapCopy.putAll( factoryMap );
+    for ( String key : keys ) {
+      OSGIResourceBundleFactory nameToFactoryEntry = factoryMap.get( key );
       String name = nameToFactoryEntry.getPropertyFilePath();
       Matcher defaultMatcher = OSGIResourceNamingConvention.getResourceNameMatcher( name );
       String defaultName = defaultMatcher.group( 1 );
-      OSGIResourceBundleFactory defaultFactory =
-        factoryMap.get( defaultName + OSGIResourceNamingConvention.RESOURCES_DEFAULT_EXTENSION );
-      OSGIResourceBundle parentBundle = null;
-      if ( defaultFactory != null && defaultFactory != nameToFactoryEntry ) {
-        parentBundle = defaultFactory.getBundle( null );
+      String locale = defaultMatcher.group( 2 );
+      if ( locale.length() > 1 ) {
+        locale = locale.substring( 1 ).replace( '_', '-' );
       }
-      resultKeyBundles = nameToFactoryEntry.getBundle( parentBundle );
+      OSGIResourceBundleFactory defaultFactory = null;
+      List<String> candidates =
+        OSGIResourceNamingConvention.getCandidateNames( defaultName, Locale.forLanguageTag( locale ) );
+      // first try to get closest parent from properties with same locale and then in properties with "lower" locale
+      for ( int i = 0; i < candidates.size(); i++ ) {
+        Optional<String> firstKeyInHierarhy = getFirstKeyInHierarhy( factoryMapCopy, candidates.get( i )
+          + OSGIResourceNamingConvention.RESOURCES_DEFAULT_EXTENSION );
+        //checks that we've found not the same properties as original
+        if ( firstKeyInHierarhy.isPresent()
+          && factoryMapCopy.get( firstKeyInHierarhy.get() ) != nameToFactoryEntry ) {
+          if ( i != candidates.size() - 1 ) {
+            defaultFactory = factoryMapCopy.remove( firstKeyInHierarhy.get() );
+          } else {
+            defaultFactory = factoryMapCopy.get( firstKeyInHierarhy.get() );
+          }
+          break;
+        }
+      }
+      OSGIResourceBundle parentBundle = null;
+      if ( defaultFactory != null ) {
+        parentBundle = result.get( defaultFactory.getBundle( null ).getDefaultName() );
+      }
+      OSGIResourceBundle resultKeyBundles = nameToFactoryEntry.getBundle( parentBundle );
       result.put( resultKeyBundles.getDefaultName(), resultKeyBundles );
     }
     return result;
+  }
+
+  /**
+   * Gets first key from SortedMap with starts with parameter name
+   *
+   * @param factoryMapWithoutCurrent map to search from
+   * @param name                     starting string
+   * @return key
+   */
+  private Optional<String> getFirstKeyInHierarhy( SortedMap<String, OSGIResourceBundleFactory> factoryMapWithoutCurrent,
+                                                  String name ) {
+    return factoryMapWithoutCurrent.keySet().stream()
+      .filter( new Predicate<String>() {
+        @Override public boolean test( String s ) {
+          return s.startsWith( name );
+        }
+      } )
+      .findFirst();
   }
 }
