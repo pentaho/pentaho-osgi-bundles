2973a4bdcfb34141aff301a6f1ee7b2705268f41
marcoslarsen <marcoslarsen@gmail.com>
2016-06-23 22:36:23 +0100

    [BACKLOG-8881] - Checkstyle and copyright fixes.

--- pentaho-ss2-proxies/src/main/java/org/pentaho/proxy/creators/ProxyUtils.java
+++ pentaho-ss2-proxies/src/main/java/org/pentaho/proxy/creators/ProxyUtils.java
@@ -1,9 +1,24 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2016 Pentaho Corporation. All rights reserved.
+ */
+
 package org.pentaho.proxy.creators;
 
 import org.pentaho.platform.engine.core.system.PentahoSystem;
-import org.pentaho.platform.proxy.api.IProxyCreator;
 import org.pentaho.platform.proxy.api.IProxyFactory;
-import org.pentaho.platform.proxy.impl.ProxyException;
 
 import java.lang.reflect.Method;
 
@@ -18,7 +33,7 @@ public class ProxyUtils {
   }
 
   public static ProxyUtils getInstance() {
-    if( singleton == null ){
+    if ( singleton == null ) {
       singleton = new ProxyUtils();
     }
     return singleton;
@@ -32,15 +47,15 @@ public class ProxyUtils {
 
   // static methods
 
-  public static Method findMethodByName( Class clazz, String methodName ){
+  public static Method findMethodByName( Class clazz, String methodName ) {
     return findMethodByName( clazz, methodName, new Class[] {} /* zero-args method */ );
   }
 
-  public static Method findMethodByName( Class clazz, String methodName, Class... arguments ){
+  public static Method findMethodByName( Class clazz, String methodName, Class... arguments ) {
     for ( Method method : clazz.getMethods() ) {
-      if( method.getName().equals( methodName ) ){
+      if ( method.getName().equals( methodName ) ) {
         for ( int i = 0; i < arguments.length; i++ ) {
-          if( method.getParameterTypes()[ i ].isAssignableFrom( arguments[i] ) == false ){
+          if ( method.getParameterTypes()[ i ].isAssignableFrom( arguments[i] ) == false ) {
             return null;
           }
         }
@@ -52,27 +67,27 @@ public class ProxyUtils {
 
   public static boolean isRecursivelySupported( final String supportedClassName, Class aClass  ) {
 
-    if( aClass != null && supportedClassName != null && !supportedClassName.isEmpty() ) {
+    if ( aClass != null && supportedClassName != null && !supportedClassName.isEmpty() ) {
 
       // 1. the class itself is the 'supportedClassName'
-      if( supportedClassName.equals( aClass.getName() ) ){
+      if ( supportedClassName.equals( aClass.getName() ) ) {
         return true;
 
       }
 
       // 2. one of its interfaces is the 'supportedClassName'
-      if( aClass.getInterfaces().length > 0 ){
+      if ( aClass.getInterfaces().length > 0 ) {
 
-        for( Class c : aClass.getInterfaces() ) {
+        for ( Class c : aClass.getInterfaces() ) {
 
-          if( c.getName().equals( supportedClassName ) ){
+          if ( c.getName().equals( supportedClassName ) ) {
             return true;
           }
         }
       }
 
       // 3. one of its superclasses is the 'supportedClassName' ( recursive check )
-      if( aClass.getSuperclass() != null ) {
+      if ( aClass.getSuperclass() != null ) {
         return isRecursivelySupported( supportedClassName, aClass.getSuperclass() );
       }
 
