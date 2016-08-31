5f2e92eac36f6f8205c363155f603a2415230e66
Jonathan Jarvis <jjarvis@.pentaho.com>
2016-08-31 12:39:27 -0500

    BISERVER-13333 - Style and License Fixes

--- pentaho-ss2-proxies/src/main/java/org/pentaho/proxy/creators/ProxyObjectBase.java
+++ pentaho-ss2-proxies/src/main/java/org/pentaho/proxy/creators/ProxyObjectBase.java
@@ -1,3 +1,26 @@
+/*! ******************************************************************************
+ *
+ * Pentaho OSGi Bundles
+ *
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+
 package org.pentaho.proxy.creators;
 
 import java.lang.reflect.InvocationTargetException;
@@ -8,44 +31,40 @@ import org.slf4j.LoggerFactory;
 
 public abstract class ProxyObjectBase {
 
-  private Logger logger = LoggerFactory.getLogger( getClass() );
+  private Logger logger = LoggerFactory.getLogger( getClass());
 
   protected Object baseTarget;
   private Method toString;
 
-  //TODO: Consider implementing equals and hashCode
+  // TODO: Consider implementing equals and hashCode
   /*
-  private Method equals;
-  private Method hashCode;
-  */
-
-  protected ProxyObjectBase(Object baseTarget) {
+   * private Method equals; private Method hashCode;
+   */
+  protected ProxyObjectBase( Object baseTarget ) {
     this.baseTarget = baseTarget;
   }
 
-  /*
-   * Note: Left implementation open so objects extending can get toString without proxy object reference
+  /**
+   * Note: Left implementation open so objects extending can get toString without proxy
+   * object reference
    */
   public String baseTargetToString() {
     try {
-      if(toString == null) {
-        toString = ProxyUtils.findMethodByName(baseTarget.getClass(), "toString");
+      if ( toString == null ) {
+        toString = ProxyUtils.findMethodByName( baseTarget.getClass(), "toString" );
       }
 
-      return (String) toString.invoke(baseTarget);
+      return (String) toString.invoke( baseTarget );
 
-    } catch(IllegalAccessException | InvocationTargetException e) {
-      logger.error(e.getMessage(), e);
+    } catch ( IllegalAccessException | InvocationTargetException e ) {
+      logger.error( e.getMessage(), e );
     }
     return super.toString();
   }
-
 
   @Override
   public String toString() {
     return super.toString() + " " + baseTargetToString();
   }
-
-
 
 }
--- pentaho-ss4-proxies/src/main/java/org/pentaho/proxy/creators/ProxyObjectBase.java
+++ pentaho-ss4-proxies/src/main/java/org/pentaho/proxy/creators/ProxyObjectBase.java
@@ -1,3 +1,25 @@
+/*! ******************************************************************************
+ *
+ * Pentaho OSGi Bundles
+ *
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.proxy.creators;
 
 import java.lang.reflect.InvocationTargetException;
@@ -13,13 +35,12 @@ public abstract class ProxyObjectBase {
   protected Object baseTarget;
   private Method toString;
 
-  //TODO: Consider implementing equals and hashCode
+  // TODO: Consider implementing equals and hashCode
   /*
-  private Method equals;
-  private Method hashCode;
-  */
+   * private Method equals; private Method hashCode;
+   */
 
-  protected ProxyObjectBase(Object baseTarget) {
+  protected ProxyObjectBase( Object baseTarget ) {
     this.baseTarget = baseTarget;
   }
 
@@ -28,24 +49,21 @@ public abstract class ProxyObjectBase {
    */
   public String baseTargetToString() {
     try {
-      if(toString == null) {
-        toString = ProxyUtils.findMethodByName(baseTarget.getClass(), "toString");
+      if ( toString == null ) {
+        toString = ProxyUtils.findMethodByName( baseTarget.getClass(), "toString" );
       }
 
-      return (String) toString.invoke(baseTarget);
+      return (String) toString.invoke( baseTarget );
 
-    } catch(IllegalAccessException | InvocationTargetException e) {
-      logger.error(e.getMessage(), e);
+    } catch ( IllegalAccessException | InvocationTargetException e ) {
+      logger.error( e.getMessage(), e );
     }
     return super.toString();
   }
-
 
   @Override
   public String toString() {
     return super.toString() + " " + baseTargetToString();
   }
-
-
 
 }
