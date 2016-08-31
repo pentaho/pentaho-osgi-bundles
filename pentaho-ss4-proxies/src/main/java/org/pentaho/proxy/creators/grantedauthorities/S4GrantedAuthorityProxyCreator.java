8c3e3270ce0343fd87d63b3fbe89be602a0be301
Jonathan Jarvis <jjarvis@.pentaho.com>
2016-08-31 16:52:56 -0500

    BISERVER-13333 License Updates and ProxyObjectBase commits

--- pentaho-ss4-proxies/src/main/java/org/pentaho/proxy/creators/grantedauthorities/S4GrantedAuthorityProxyCreator.java
+++ pentaho-ss4-proxies/src/main/java/org/pentaho/proxy/creators/grantedauthorities/S4GrantedAuthorityProxyCreator.java
@@ -1,4 +1,26 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2016 Pentaho Corporation.  All rights reserved.
+ */
+
+
 package org.pentaho.proxy.creators.grantedauthorities;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.pentaho.platform.proxy.api.IProxyCreator;
 import org.pentaho.platform.proxy.api.IProxyFactory;
@@ -8,20 +30,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class S4GrantedAuthorityProxyCreator implements IProxyCreator<GrantedAuthority> {
 
   private Logger logger = LoggerFactory.getLogger( getClass() );
 
-  @Override public boolean supports( Class aClass ) {
+  @Override
+  public boolean supports( Class aClass ) {
     // supports legacy spring.security 2.0.8 SecurityContext
     return ProxyUtils.isRecursivelySupported( "org.springframework.security.GrantedAuthority", aClass );
   }
 
-  @Override public GrantedAuthority create( Object o ) {
+  @Override
+  public GrantedAuthority create( Object o ) {
     return new ProxyGrantedAuthority( o );
   }
 
@@ -39,7 +59,7 @@ public class S4GrantedAuthorityProxyCreator implements IProxyCreator<GrantedAuth
     private Method getAuthorityMethod;
 
     public ProxyGrantedAuthority( Object target ) {
-      super(target);
+      super( target );
     }
 
     @Override
@@ -53,12 +73,11 @@ public class S4GrantedAuthorityProxyCreator implements IProxyCreator<GrantedAuth
         return (String) getAuthorityMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return null;
     }
-
 
   }
 
--- pentaho-ss4-proxies/src/main/java/org/pentaho/proxy/creators/userdetailsservice/S4UserDetailsProxyCreator.java
+++ pentaho-ss4-proxies/src/main/java/org/pentaho/proxy/creators/userdetailsservice/S4UserDetailsProxyCreator.java
@@ -1,4 +1,28 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2016 Pentaho Corporation.  All rights reserved.
+ */
+
+
 package org.pentaho.proxy.creators.userdetailsservice;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.pentaho.platform.proxy.api.IProxyCreator;
 import org.pentaho.proxy.creators.ProxyObjectBase;
@@ -9,21 +33,18 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.ReflectionUtils;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collection;
-
 public class S4UserDetailsProxyCreator implements IProxyCreator<UserDetails> {
 
   private Logger logger = LoggerFactory.getLogger( getClass() );
 
-  @Override public boolean supports( Class<?> clazz ) {
+  @Override
+  public boolean supports( Class<?> clazz ) {
     // supports legacy spring.security 2.0.8 UserDetails
     return "org.springframework.security.userdetails.UserDetails".equals( clazz.getName() );
   }
 
-  @Override public UserDetails create( Object target ) {
+  @Override
+  public UserDetails create( Object target ) {
     return new S4UserDetailsProxy( target );
   }
 
@@ -37,14 +58,14 @@ public class S4UserDetailsProxyCreator implements IProxyCreator<UserDetails> {
     private Method isCredentialsNonExpiredMethod;
     private Method isEnabledMethod;
 
-
-    public S4UserDetailsProxy( Object target ){
-      super(target);
+    public S4UserDetailsProxy( Object target ) {
+      super( target );
     }
 
-    @Override public Collection<? extends GrantedAuthority> getAuthorities() {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
 
-      if( getAuthoritiesMethod == null ){
+      if ( getAuthoritiesMethod == null ) {
         getAuthoritiesMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "getAuthorities" );
       }
 
@@ -54,122 +75,128 @@ public class S4UserDetailsProxyCreator implements IProxyCreator<UserDetails> {
 
         Object authoritiesObj = getAuthoritiesMethod.invoke( baseTarget );
 
-        if( authoritiesObj != null && authoritiesObj instanceof Object[] ){
+        if ( authoritiesObj != null && authoritiesObj instanceof Object[] ) {
 
-          for( Object authorityObj : ( Object[] ) authoritiesObj ){
+          for ( Object authorityObj : (Object[]) authoritiesObj ) {
 
             Method getAuthority = ReflectionUtils.findMethod( authorityObj.getClass(), "getAuthority" );
             Object authority = getAuthority.invoke( authorityObj );
-            if( authority != null ) {
+            if ( authority != null ) {
               s4Authorities.add( new SimpleGrantedAuthority( authority.toString() ) );
             }
           }
         }
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return s4Authorities;
     }
 
-    @Override public String getPassword() {
+    @Override
+    public String getPassword() {
 
-      if( getPasswordMethod == null ){
+      if ( getPasswordMethod == null ) {
         getPasswordMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "getPassword" );
       }
 
       try {
 
-        return ( String ) getPasswordMethod.invoke( baseTarget );
+        return (String) getPasswordMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return null;
     }
 
-    @Override public String getUsername() {
+    @Override
+    public String getUsername() {
 
-      if( getUsernameMethod == null ){
+      if ( getUsernameMethod == null ) {
         getUsernameMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "getUsername" );
       }
 
       try {
 
-        return ( String ) getUsernameMethod.invoke( baseTarget );
+        return (String) getUsernameMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return null;
     }
 
-    @Override public boolean isAccountNonExpired() {
+    @Override
+    public boolean isAccountNonExpired() {
 
-      if( isAccountNonExpiredMethod == null ){
+      if ( isAccountNonExpiredMethod == null ) {
         isAccountNonExpiredMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "isAccountNonExpired" );
       }
 
       try {
 
-        return ( Boolean ) isAccountNonExpiredMethod.invoke( baseTarget );
+        return (Boolean) isAccountNonExpiredMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return false;
     }
 
-    @Override public boolean isAccountNonLocked() {
+    @Override
+    public boolean isAccountNonLocked() {
 
-      if( isAccountNonLockedMethod == null ){
+      if ( isAccountNonLockedMethod == null ) {
         isAccountNonLockedMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "isAccountNonLocked" );
       }
 
       try {
 
-        return ( Boolean ) isAccountNonLockedMethod.invoke( baseTarget );
+        return (Boolean) isAccountNonLockedMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return false;
     }
 
-    @Override public boolean isCredentialsNonExpired() {
+    @Override
+    public boolean isCredentialsNonExpired() {
 
-      if( isCredentialsNonExpiredMethod == null ){
+      if ( isCredentialsNonExpiredMethod == null ) {
         isCredentialsNonExpiredMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "isCredentialsNonExpired" );
       }
 
       try {
 
-        return ( Boolean ) isCredentialsNonExpiredMethod.invoke( baseTarget );
+        return (Boolean) isCredentialsNonExpiredMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return false;
     }
 
-    @Override public boolean isEnabled() {
+    @Override
+    public boolean isEnabled() {
 
-      if( isEnabledMethod == null ){
+      if ( isEnabledMethod == null ) {
         isEnabledMethod = ReflectionUtils.findMethod( baseTarget.getClass(), "isEnabled" );
       }
 
       try {
 
-        return ( Boolean ) isEnabledMethod.invoke( baseTarget );
+        return (Boolean) isEnabledMethod.invoke( baseTarget );
 
       } catch ( InvocationTargetException | IllegalAccessException e ) {
-        logger.error( e.getMessage() , e );
+        logger.error( e.getMessage(), e );
       }
 
       return false;
