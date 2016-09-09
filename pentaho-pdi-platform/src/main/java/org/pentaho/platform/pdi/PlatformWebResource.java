5564d21e2f58384d41b456c128c0cd74d3d2e17a
Nick Baker <nbaker@pentaho.com>
2016-09-08 22:54:20 -0400

    [BACKLOG-10022] Registering ExternalResources in Blueprint

---
+++ pentaho-pdi-platform/src/main/java/org/pentaho/platform/pdi/PlatformWebResource.java
@@ -0,0 +1,42 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2016 Pentaho Corporation..  All rights reserved.
+ */
+package org.pentaho.platform.pdi;
+
+import org.pentaho.platform.api.engine.IPlatformWebResource;
+
+/**
+ * Simple bean implementing IPlatformWebResource
+ *
+ * Created by nbaker on 9/8/16.
+ */
+public class PlatformWebResource implements IPlatformWebResource {
+
+  private final String context, location;
+
+  public PlatformWebResource( String context, String location ) {
+    this.context = context;
+    this.location = location;
+  }
+
+  @Override public String getContext() {
+    return context;
+  }
+
+  @Override public String getLocation() {
+    return location;
+  }
+}
--- pentaho-platform-plugin-deployer/src/test/java/org/pentaho/osgi/platform/plugin/deployer/impl/handlers/pluginxml/PluginXmlExternalResourcesHandlerTest.java
+++ pentaho-platform-plugin-deployer/src/test/java/org/pentaho/osgi/platform/plugin/deployer/impl/handlers/pluginxml/PluginXmlExternalResourcesHandlerTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2014 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2014-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -26,13 +26,18 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.Equals;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.pentaho.osgi.platform.plugin.deployer.api.PluginHandlingException;
 import org.pentaho.osgi.platform.plugin.deployer.api.PluginMetadata;
 import org.pentaho.osgi.platform.plugin.deployer.impl.JSONUtil;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -41,8 +46,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 /**
@@ -89,6 +97,20 @@ public class PluginXmlExternalResourcesHandlerTest {
       }
     } ).when( fileWriter ).write( anyString() );
     when( pluginMetadata.getFileWriter( PluginXmlExternalResourcesHandler.EXTERNAL_RESOURCES_FILE ) ).thenReturn( fileWriter );
+
+
+    // Setup Blueprint
+    Document blueprint = null;
+    try {
+      blueprint = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+      blueprint.appendChild( blueprint.createElementNS( PluginXmlStaticPathsHandler.BLUEPRINT_BEAN_NS, "blueprint" ) );
+    } catch ( ParserConfigurationException e ) {
+      e.printStackTrace();
+      fail();
+    }
+    when( pluginMetadata.getBlueprint() ).thenReturn( blueprint );
+
+
     List<Node> nodes = new ArrayList<Node>( Arrays.asList( node1, node2 ) );
     pluginXmlExternalResourcesHandler.handle( "plugin.xml", nodes, pluginMetadata );
     String result = sb.toString();
@@ -99,5 +121,12 @@ public class PluginXmlExternalResourcesHandlerTest {
     assertEquals( 2, configs.size() );
     assertEquals( "/test/content/1", configs.get( 0 ) );
     assertEquals( "/test/content/2", configs.get( 1 ) );
+
+    NodeList childNodes = blueprint.getDocumentElement().getChildNodes();
+    assertEquals( 2, childNodes.getLength() );
+    for ( int i = 0; i < childNodes.getLength(); i++ ) {
+      assertEquals( "service", childNodes.item( i ).getNodeName() );
+      assertEquals( "org.pentaho.platform.pdi.PlatformWebResource", childNodes.item( i ).getFirstChild().getAttributes().getNamedItem( "class" ).getNodeValue() );
+    }
   }
 }
