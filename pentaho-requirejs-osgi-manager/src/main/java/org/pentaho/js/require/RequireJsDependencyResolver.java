ca379860cb9f85b296e3db310aff0092c618f565
Nelson Antunes <nelson.antunes@webdetails.pt>
2017-02-10 15:29:03 +0000

    [BACKLOG-13736] Copyright header

--- pentaho-requirejs-osgi-manager/src/main/java/org/pentaho/js/require/RequireJsDependencyResolver.java
+++ pentaho-requirejs-osgi-manager/src/main/java/org/pentaho/js/require/RequireJsDependencyResolver.java
@@ -1,3 +1,25 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2015-2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.js.require;
 
 import com.github.zafarkhaja.semver.Version;
@@ -12,9 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created by nantunes on 12/11/15.
- */
 public class RequireJsDependencyResolver {
   public static void processMetaInformation( Map<String, Object> result ) {
     if ( !result.containsKey( "requirejs-osgi-meta" ) ) {
