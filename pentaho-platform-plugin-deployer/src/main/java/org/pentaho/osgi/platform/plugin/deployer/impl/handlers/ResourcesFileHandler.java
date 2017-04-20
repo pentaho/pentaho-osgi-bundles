9f854761378be7f02af8e5337f98b43601fb332b
Joao Figueiredo <jfigueiredo@pentaho.com>
2017-04-20 15:41:36 +0100

    Rebase the Artifact-prototype to master branch last commit.

--- pentaho-platform-plugin-deployer/src/main/java/org/pentaho/osgi/platform/plugin/deployer/impl/handlers/ResourcesFileHandler.java
+++ pentaho-platform-plugin-deployer/src/main/java/org/pentaho/osgi/platform/plugin/deployer/impl/handlers/ResourcesFileHandler.java
@@ -4,33 +4,42 @@ import org.pentaho.osgi.platform.plugin.deployer.api.PluginFileHandler;
 import org.pentaho.osgi.platform.plugin.deployer.api.PluginHandlingException;
 import org.pentaho.osgi.platform.plugin.deployer.api.PluginMetadata;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.regex.Pattern;
 
 /**
- * Created by joao.figueiredo on 02/04/17.
+ * Created by jfigueiredo on 02/04/2017.
  */
 public class ResourcesFileHandler implements PluginFileHandler {
 
-  public static final Pattern LIB_PATTERN = Pattern.compile( ".+\\/resources\\/.*\\.properties" );
+    public static final Pattern LIB_PATTERN = Pattern.compile(".+\\/resources\\/.*\\.properties");
 
-  @Override public boolean handles( String fileName ) {
-    return LIB_PATTERN.matcher( fileName ).matches();
-  }
-
-  @Override public void handle( String relativePath, File file, PluginMetadata pluginMetadata ) throws PluginHandlingException {
-    try ( FileReader fileReader = new FileReader( file );
-          FileWriter fileWriter = pluginMetadata.getFileWriter( relativePath.substring( relativePath.indexOf( "/resources/" ) + 1 ) ); ) {
-      int read;
-      while ( ( read = fileReader.read() ) != -1 ) {
-        fileWriter.write( read );
-      }
-    } catch ( IOException e ) {
-      e.printStackTrace();
+    @Override
+    public boolean handles(String fileName) {
+        return LIB_PATTERN.matcher(fileName).matches();
     }
-  }
-}
 
+    @Override
+    public boolean handle(String relativePath, byte[] file, PluginMetadata pluginMetadata) throws PluginHandlingException {
+
+        OutputStream outputStream = null;
+        try {
+            outputStream = pluginMetadata.getFileOutputStream(relativePath.substring(relativePath.indexOf("/resources/") + 1));
+            outputStream.write(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(outputStream!=null){
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return false;
+    }
+}
