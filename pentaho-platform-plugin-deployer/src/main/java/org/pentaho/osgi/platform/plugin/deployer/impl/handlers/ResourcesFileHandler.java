38d19a96c13bd91d68a972900760f713a36a5e4c
Joao Figueiredo <jfigueiredo@pentaho.com>
2017-04-26 13:36:06 +0100

    Simplify the resource files handler, and upgrade the code to cope with the latest API changes.

--- pentaho-platform-plugin-deployer/src/main/java/org/pentaho/osgi/platform/plugin/deployer/impl/handlers/ResourcesFileHandler.java
+++ pentaho-platform-plugin-deployer/src/main/java/org/pentaho/osgi/platform/plugin/deployer/impl/handlers/ResourcesFileHandler.java
@@ -23,21 +23,10 @@ public class ResourcesFileHandler implements PluginFileHandler {
     @Override
     public boolean handle(String relativePath, byte[] file, PluginMetadata pluginMetadata) throws PluginHandlingException {
 
-        OutputStream outputStream = null;
-        try {
-            outputStream = pluginMetadata.getFileOutputStream(relativePath.substring(relativePath.indexOf("/resources/") + 1));
-            outputStream.write(file);
-
+        try (OutputStream outputStream = pluginMetadata.getFileOutputStream(relativePath.substring(relativePath.indexOf("/resources/") + 1))){
+           outputStream.write(file);
         } catch (IOException e) {
             e.printStackTrace();
-        }finally{
-            if(outputStream!=null){
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
 
         return false;
