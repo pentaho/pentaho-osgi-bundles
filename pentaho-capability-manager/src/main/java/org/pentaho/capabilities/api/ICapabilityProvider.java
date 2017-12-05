ebcdb88344005bae687943afa2191209ed016687
Chris Caspanello <ccaspanello@pentaho.com>
2017-12-05 10:19:40 -0500

    [BACKLOG-20260]  Fixing check-style issues.

--- pentaho-capability-manager/src/main/java/org/pentaho/capabilities/api/ICapabilityProvider.java
+++ pentaho-capability-manager/src/main/java/org/pentaho/capabilities/api/ICapabilityProvider.java
@@ -1,3 +1,24 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 package org.pentaho.capabilities.api;
 
 import java.util.Set;
@@ -6,7 +27,7 @@ import java.util.Set;
  * Implementations of this class provide access to various system Capabilities. There's no explicit SPI, but
  * implementations of ICapabilityManager need to have some way to find these providers, or they must be registered
  * with the Manager in some way.
- *
+ * <p>
  * Created by nbaker on 4/6/15.
  */
 public interface ICapabilityProvider {
@@ -20,6 +41,7 @@ public interface ICapabilityProvider {
 
   /**
    * Get a list of Capability IDs found by the provider
+   *
    * @return
    */
   Set<String> listCapabilities();
@@ -39,7 +61,7 @@ public interface ICapabilityProvider {
    * @param id
    * @return
    */
-  boolean capabilityExist(String id);
+  boolean capabilityExist( String id );
 
   /**
    * Get a set containing all ICapabilities
