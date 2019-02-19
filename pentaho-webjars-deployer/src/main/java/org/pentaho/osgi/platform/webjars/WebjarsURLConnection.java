75e9df062327aaee894e710863c33d1f4b7ab993
Nelson Antunes <nelson.antunes@hitachivantara.com>
2019-02-19 16:24:37 +0000

    [BACKLOG-27574] Remove Felix HTTP dependency

--- pentaho-webjars-deployer/src/main/java/org/pentaho/osgi/platform/webjars/WebjarsURLConnection.java
+++ pentaho-webjars-deployer/src/main/java/org/pentaho/osgi/platform/webjars/WebjarsURLConnection.java
@@ -503,7 +503,7 @@ public class WebjarsURLConnection extends URLConnection {
           .put( new Attributes.Name( Constants.BUNDLE_SYMBOLICNAME ), "pentaho-webjars-" + artifactInfo.getArtifactId() );
       manifest.getMainAttributes()
           .put( new Attributes.Name( Constants.IMPORT_PACKAGE ),
-              "org.osgi.service.http,org.apache.felix.http.api,org.ops4j.pax.web.extender.whiteboard.runtime,"
+              "org.osgi.service.http,org.ops4j.pax.web.extender.whiteboard.runtime,"
                   + "org.ops4j.pax.web.extender.whiteboard" );
 
       manifest.getMainAttributes()
