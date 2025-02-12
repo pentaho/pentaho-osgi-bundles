b03d25d4aecca434d16505b293e9a9a0c63baea8
Vamshi Rajidi <vamshi.rajidi@hitachivantara.com>
2025-02-12 17:52:39 +0530

    [BACKLOG-43209]-OSGi Assembly issues

--- pentaho-pdi-platform/src/main/java/org/pentaho/platform/pdi/WebContextServlet.java
+++ pentaho-pdi-platform/src/main/java/org/pentaho/platform/pdi/WebContextServlet.java
@@ -19,10 +19,10 @@ import org.pentaho.di.core.Const;
 import org.pentaho.di.i18n.LanguageChoice;
 import org.pentaho.platform.api.engine.IPlatformWebResource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
